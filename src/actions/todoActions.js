dc89cd6e83554640156ba2af4bc5eeb329a7c536
Jeferson Leao <jeferson.leao@b2wdigital.com>
2015-12-04 17:51:18 -0200

    gh-pages

--- src/actions/todoActions.js
+++
@@ -1,5 +0,0 @@
-import {createActions} from '../../framework'
-
-export default createActions([
-  'CHANGE_ADD_TODO_TEXT', 'CHANGE_TODOS', 'SAVE_TODO', 'FINISH_TODO', 'RESUME_TODO', 'CLEAR_DONE'
-])
--- src/channels.js
+++
@@ -1,6 +0,0 @@
-import {createChans} from '../framework'
-
-export default createChans([
-  'onPersist',
-  'onChangeTodoText', 'onSubmitTodoForm', 'onClearDone', 'onFinishTodo', 'onResumeTodo'
-])
